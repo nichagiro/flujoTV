5eba8d0fcec9182c51d4bf0e6686c2358c573067
Nicolas Chamorro <nichagiro@gmail.com>
2025-11-26 00:15:43 -0500

    <Descargar /> update LINK

--- containers/home/Descargar.jsx
+++ containers/home/Descargar.jsx
@@ -1,15 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Descargar = ({ className = 'mt-2 dg-red px-5 py-3' }) => {
   return (
     <section>
-      <button
+      <Link
         // data-toggle="modal"
         // data-target="#app_magis"
+        href={`https://wa.me/${ process.env.NEXT_PUBLIC_WHATSAPP_COUNTRY}${process.env.NEXT_PUBLIC_WHATSAPP_NUMBER}`}
+        target='whatsaap'
         className={`btn-download ${className}`}
       >
         Asesor Técnico
-      </button>
+      </Link>
     </section>)
 }
 
